f5888bf9f8ab836f46ed3152fa4e1691529cb436
vpquieng <vincent.quieng@mph-intl.com>
2025-10-13 16:26:09 +0800

    Initial

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className='flex-1 items-center justify-center bg-rose-500'>
+        <Text className='text-white text-3xl font-bold'>Countdown Event App</Text>
+    </View>
+  );
+}
+
