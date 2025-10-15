60fa005751cdd91324f4140a846331938c3863ba
vpquieng <vincent.quieng@mph-intl.com>
2025-10-15 15:16:34 +0800

    adjust add button component

--- app/components/add-event-button.tsx
+++ app/components/add-event-button.tsx
@@ -3,15 +3,18 @@ import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
-export default function addButton() {
+export default function AddButton() {
   const router = useRouter();
 
-    return (
-        <TouchableOpacity
-            onPress={() => router.push('/tabs/add-event')}
-            className="text-gray-800 text-lg font-semibold"
-        >
-            <Ionicons name="add" size={40} color="black" />
-        </TouchableOpacity>
-    )
+  return (
+    <View className="w-[90%] p-4 mb-4 bg-white rounded-2xl flex-row items-center justify-between">
+      <TouchableOpacity
+        onPress={() => router.push('/tabs/add-event')}
+        activeOpacity={0.8}
+        className="flex-1 items-center justify-center"
+      >
+        <Ionicons name="add" size={40} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
 }
